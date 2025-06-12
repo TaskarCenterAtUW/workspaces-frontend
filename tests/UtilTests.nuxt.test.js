759e8d4e1c3cf26e2a64fcc1f8d53f5a778dd1f4
uwjohnbee <johnbee@uw.edu>
2025-06-12 14:30:57 -0400

    fixed unit test, now pass!

--- tests/UtilTests.nuxt.test.js
+++ tests/UtilTests.nuxt.test.js
@@ -1,13 +1,13 @@
 // @vitest-environment nuxt
-import { test } from 'vitest'
-import { pullAttributesByName } from '../util/xml.js'
+import { expect, test } from 'vitest'
+import { pullAttributesByName } from '~/util/xml'
 
 test('XML pullAttributesByName test', () => {
     const xmlString = `<example><item id="4">Howdy</item><item id="5">There</from></example>`;
     
-    const attributeList = pullAttributesByName(xmlString, 'item', 'id');
+    const attributeList = pullAttributesByName(xmlString, 'id', 'item');
     expect(attributeList).toEqual(['4', '5']);
 
-    const missingAttributeList = pullAttributesByName(xmlString, 'item', 'missing');
+    const missingAttributeList = pullAttributesByName(xmlString, 'missing', 'id');
     expect(missingAttributeList).toEqual([]);
 })
