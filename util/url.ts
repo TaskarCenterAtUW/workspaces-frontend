7d9614b5649877ca4dd5a1b4ee43773ecd760dff
Cy Rossignol <cy@rossignols.me>
2025-10-14 20:31:19 -0700

    Add support for long form quest URLs

---
+++ util/url.ts
@@ -0,0 +1,15 @@
+export function isHttpUrl(input: any): boolean {
+  let url: URL;
+
+  try {
+    url = new URL(input);
+  } catch {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
+export function normalizeUrl(input: string): string {
+  return new URL(input).href
+}
