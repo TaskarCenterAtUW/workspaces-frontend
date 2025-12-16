2306117cfa67e79068c3c66b68f258328dee7dd4
Jeff Maki <hello@jeffmaki.com>
2025-12-16 17:13:20 -0500

    Update

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,4 +2,9 @@ import * as Sentry from '@sentry/nuxt';
 
 Sentry.init({
   dsn: "https://59072b36b884fee8ff9a54c33b3c4557@o4510431738200064.ingest.us.sentry.io/4510546162614272",
+  integrations: [
+    Sentry.consoleLoggingIntegration({
+      levels: ["log", "warn", "error"],
+    }),
+  ],
 });
