15f61239150e7e0d2241f081582fc0c4be082b0c
Jeff Maki <hello@jeffmaki.com>
2026-03-13 10:16:25 -0400

    Revised dev setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  sourcemap: { client: 'hidden' },
-  devtools: { enabled: true },
   modules: [
     '@bootstrap-vue-next/nuxt',
     '@nuxt/eslint',
     '@sentry/nuxt/module',
   ],
-  nitro: {
-    // deal with CORS issues during development
-    devProxy: {
-      '/api': 'http://localhost:8000/api/',
-      '/workspaces': 'http://localhost:8000/workspaces/',
-    }
-  },
+  ssr: false,
+  devtools: { enabled: true },
   app: {
     head: {
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
         { rel: 'stylesheet', href: 'https://material-icons.github.io/material-icons-font/css/outline.css' },
-        { rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css' }
+        { rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css' },
       ],
       script: [
         { src: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js' },
-      ]
+      ],
     },
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
   css: [
-    '~/assets/scss/main.scss'
+    '~/assets/scss/main.scss',
   ],
+  sourcemap: { client: 'hidden' },
   compatibilityDate: '2024-10-24',
+
+  /*
+  nitro: {
+    // deal with CORS issues during development--not sure this is always required, or required at all?
+    devProxy: {
+      // use these values when you want to use the existing dev backend
+      '/api': 'https://api.workspaces-dev.sidewalks.washington.edu/api/',
+      '/workspaces': 'https://osm.workspaces-dev.sidewalks.washington.edu/workspaces/',
+    },
+  },
+  */
+
   eslint: {
     config: {
-      stylistic: true
-    }
+      stylistic: true,
+    },
   },
   sentry: {
     org: 'taskar-center-at-uw',
