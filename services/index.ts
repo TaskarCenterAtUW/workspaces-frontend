f70c91e04ec72e628e3533c8410cce5b9c9da3bd
Jeff Maki <hello@jeffmaki.com>
2026-07-08 17:20:20 -0400

    Update index.ts

--- services/index.ts
+++ services/index.ts
@@ -19,9 +19,7 @@ import { WorkspacesClient } from '~/services/workspaces';
 const tdeiApiUrl = import.meta.env.VITE_TDEI_API_URL;
 const tdeiUserApiUrl = import.meta.env.VITE_TDEI_USER_API_URL;
 const apiUrl = import.meta.env.VITE_API_URL;
-const newApiUrl = import.meta.env.VITE_NEW_API_URL||'https://new-api.workspaces-dev.sidewalks.washington.edu/api/v1/'
-// import.meta.env.VITE_NEW_API_URL
-//   || 'https://new-api.workspaces-dev.sidewalks.washington.edu/api/v1/';
+const newApiUrl = import.meta.env.VITE_NEW_API_URL || 'https://new-api.workspaces-dev.sidewalks.washington.edu/api/v1/'
 const osmWebUrl = import.meta.env.VITE_OSM_URL;
 const osmApiUrl = osmWebUrl + 'api/0.6/';
 const rapidUrl = import.meta.env.VITE_RAPID_URL;
