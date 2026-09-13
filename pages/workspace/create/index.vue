8820b990846c383cc77a4fe34a9270baa05f200e
Faiz <faiz@Faizs-MacBook-Pro.local>
2026-09-13 23:59:00 +0530

    Create workspace UI update

--- pages/workspace/create/index.vue
+++ pages/workspace/create/index.vue
@@ -11,35 +11,36 @@
 // @test e2e: if an API error occurs when creating a workspace from either form, an error message is shown
 
 <template>
-  <app-page>
-    <div class="text-center mt-5">
-      <app-icon
-        variant="add_to_queue"
-        size="48"
-      />
-    </div>
-    <h1 class="mb-5 text-center">Create a Workspace</h1>
-
+  <app-page class="create-container">
+    <div class="create-container-header">
+      <img
+          :src="createIcon"
+          class="create-icon"
+          alt=""
+        >
+        <h1 class="create-heading">Create a Workspace</h1>
+        <p class="create-sub-heading">Choose how you want to create your workspace. Each option offers different ways to get started.</p>
+    </div> 
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
-        <div class="card">
+        <div class="card create-category">
           <div class="card-body">
-            <app-icon
-              variant="create_new_folder"
-              size="36"
-              class="mb-2"
-            />
-            <h5 class="card-title">Blank Workspace</h5>
-            <p class="card-text">Create an empty workspace with no data.</p>
+            <img
+              :src="createBlankIcon"
+              class="create-category-icon"
+              alt=""
+            >
+            <div class="card-title">Blank Workspace</div>
+            <div class="card-text">Create an empty workspace with no data.</div>
           </div>
           <div class="card-footer">
             <nuxt-link
               to="/workspace/create/blank"
-              class="btn btn-primary"
+              class="btn btn-primary btn-stretch clr_blank"
             >
-              Start
+              Create Blank Workspace
               <app-icon
-                variant="arrow_circle_right"
+                variant="chevron_right"
                 no-margin
               />
             </nuxt-link>
@@ -48,26 +49,26 @@
       </div>
 
       <div class="col">
-        <div class="card">
+        <div class="card create-category">
           <div class="card-body">
-            <app-icon
-              variant="cloud_sync"
-              size="36"
-              class="mb-2"
-            />
-            <h5 class="card-title">From TDEI</h5>
-            <p class="card-text">
+            <img
+              :src="createTdeiIcon"
+              class="create-category-icon"
+              alt=""
+            >
+            <div class="card-title">From TDEI</div>
+            <div class="card-text">
               Create a workspace by importing an asset stored in the TDEI.
-            </p>
+            </div>
           </div>
           <div class="card-footer">
             <nuxt-link
               to="/workspace/create/tdei"
-              class="btn btn-primary"
+              class="btn btn-primary btn-stretch clr_tdei"
             >
-              Start
+              Create From TDEI
               <app-icon
-                variant="arrow_circle_right"
+                variant="chevron_right"
                 no-margin
               />
             </nuxt-link>
@@ -76,26 +77,26 @@
       </div>
 
       <div class="col">
-        <div class="card">
+        <div class="card create-category">
           <div class="card-body">
-            <app-icon
-              variant="upload_file"
-              size="36"
-              class="mb-2"
-            />
-            <h5 class="card-title">From File</h5>
-            <p class="card-text">
+            <img
+              :src="createFileIcon"
+              class="create-category-icon"
+              alt=""
+            >
+            <div class="card-title">From File</div>
+            <div class="card-text">
               Create a workspace from an OpenSidewalks data file on your computer.
-            </p>
+            </div>
           </div>
           <div class="card-footer">
             <nuxt-link
               to="/workspace/create/file"
-              class="btn btn-primary"
+              class="btn btn-primary btn-stretch clr_file"
             >
-              Start
+              Create From File
               <app-icon
-                variant="arrow_circle_right"
+                variant="chevron_right"
                 no-margin
               />
             </nuxt-link>
@@ -105,3 +106,102 @@
     </div>
   </app-page>
 </template>
+
+<script setup lang="ts">
+import createIcon from '~/assets/img/create-icon.svg';
+import createBlankIcon from '~/assets/img/create_blank.svg';
+import createTdeiIcon from '~/assets/img/create_tdei.svg';
+import createFileIcon from '~/assets/img/create_file.svg';
+
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/theme.scss";
+.create-container {
+  border: 1px solid $gray-300;
+  border-radius: 20px;
+  padding: 50px 50px !important;
+  width: 80%;
+  max-width: 1000px;
+  box-shadow: 0px 3px 6px #0000000D;
+  margin-top: 50px;
+}
+.create-container-header {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 25px;
+  .create-heading {
+    font-size: 24px;
+    font-weight: 600;
+    color: $text-navy;
+    margin-bottom: -2px;
+  }
+  .create-sub-heading {
+    font-size: 16px;
+    color: $text-secondary;
+  }
+}
+.create-icon {
+  width: 50px;
+}
+.create-category {
+  box-shadow: 0px 1px 6px #0000000D;
+  border: 1px solid #E2E4ED;
+  border-radius: 15px;
+}
+.btn-stretch {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+  padding: 8px 12px !important;
+}
+.create-category-icon {
+  width: 50px;
+  margin-bottom: 15px;
+}
+.card-title {
+  font-size: 16px;
+  font-weight: 700;
+  color: $text-navy;
+  margin-bottom: 10px;
+}
+.card-text {
+  font-size: 14px;
+  color: $text-secondary;
+  font-weight: 500;
+}
+.card-body {
+  padding: 20px 15px;
+}
+.card-footer {
+    border-top: 1px dashed #cbcbcd;
+    padding: 15px 15px;
+    background: none !important;
+}
+.clr_blank {
+  background-color: #7958D2 !important;
+  color: #ffffff !important;
+  border: none !important;
+}
+.clr_tdei {
+  background-color: #436ECE !important;
+  color: #ffffff !important;
+  border: none !important;
+}
+.clr_file {
+  background-color: #0D8696 !important;
+  color: #ffffff !important;
+  border: none !important;
+}
+@include media-breakpoint-down(md) {
+  .create-container {
+    border: none;
+    box-shadow: none;
+    padding: 40px 40px !important;
+    margin-top: 0px;
+    width: unset;
+  }
+}
+</style>
